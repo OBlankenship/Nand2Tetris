f2aadb1ce5f37624b834a8a5ae5534542c7c8ea9
Oliver Blankenship <oliverblankenship@gmail.com>
2022-12-30 15:06:28 -0600

    Implemented mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,53 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Assign RAM[0] to multiplicand variable
+@R0
+D=M
+@multiplicand
+M=D
+
+// Assign RAM[1] to multiplier variable
+@R1
+D=M
+@multiplier
+M=D
+
+// Initialize result variable
+@result
+M=0
+
+// Core multiplication loop
+(LOOP)
+
+// Check if multiplier is 0, if so calculation is complete
+@multiplier
+D=M
+@END
+D;JEQ
+
+// Decrement the multiplier
+@multiplier
+M=M-1
+
+// Add multiplicand to result
+@result
+D=M
+@multiplicand
+D=D+M
+@result
+M=D
+
+// Jump to start of core multiplication loop
+@LOOP
+0;JMP
+
+// Store results in RAM[2], and infinite loop
+(END)
+@result
+D=M
+@R2
+M=D
+@END
+0;JMP
